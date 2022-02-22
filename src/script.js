feeb1c2958143d51ff1a66ba78a875d7533a55da
hnusratullah <96744245+hnusratullah@users.noreply.github.com>
2022-02-21 22:40:14 -0600

    added background gradient

--- src/script.js
+++ src/script.js
@@ -59,10 +59,8 @@ forecastElement.innerHTML = forecastHTML;
 }
 
 function getForecast(coordinates){
-  console.log(coordinates);
   let apiKey = "74735326822fd761ab3cae03ca68c8d0";
   let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
-  console.log(apiURL);
   axios.get(apiURL).then(displayForecast);
 }
 function showWeather(response){
